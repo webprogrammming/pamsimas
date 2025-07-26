437a1e7ddc83213582ff0f950be18b54cfcf87fc
wahyuazizi <wahyuazizi03@gmail.com>
2025-07-26 22:09:26 +0000

    edit controler

--- app/Http/Controllers/PemakaianController.php
+++ app/Http/Controllers/PemakaianController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use App\Models\periode;
+use App\Models\Periode;
 use App\Models\Tarif;
 use App\Models\Pemakaian;
 use Illuminate\Http\Request;
